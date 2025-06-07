2f7456db683aace6da26081be2f077bf8d558c1f
Gtznn10 <guilhermedalmagro0416@gmail.com>
2025-06-07 20:52:59 -0300

    Creat page clothes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { StorePageComponent } from "./pages/store-page/store-page.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  selector: "app-root",
+  imports: [RouterOutlet, StorePageComponent],
+  templateUrl: "./app.html",
+  styleUrl: "./app.scss",
 })
 export class App {
-  protected title = 'clothes-store';
+  protected title = "clothes-store";
 }
